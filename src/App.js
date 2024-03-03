5d0e4568e694a9c8eaada34febedc7a188d81b92
shiba_1234 <shibajisahu121@gmail.com>
2024-03-04 00:08:55 +0530

    final implementation done without css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import OpenAI from "openai";
 import React, { useState } from "react";
+import DotPlus from "./DotPlus";
+import { Markup } from "interweave";
 
 function App() {
   const [file, setFile] = useState(null);
   const [content, setContent] = useState(null);
   const [response, setResponse] = useState(null);
+  const [clicked, setClicked] = useState(false);
 
   const handleFileChange = async (e) => {
     setFile(e.target.files[0]);
@@ -27,8 +30,9 @@ function App() {
       return;
     }
     let additionalContent = content.concat(
-      "\n Convert XML content to Rest Assured JAVA Code"
+      "\n Give me Only Code of Rest Assured JAVA using the xml content"
     );
+    setClicked(true);
     setContent(additionalContent);
     console.log("chat completion");
     const openai = new OpenAI({
@@ -36,11 +40,31 @@ function App() {
       dangerouslyAllowBrowser: true,
     });
     const completion = await openai.chat.completions.create({
-      messages: [{ role: "system", content: content }],
+      messages: [
+        {
+          role: "system",
+          content:
+            "You are a helpful assistant designed to Output Rest Assured Code in JAVA only",
+        },
+        { role: "user", content: content },
+      ],
       model: "gpt-3.5-turbo",
+      temperature: 1,
+      top_p: 1,
+      frequency_penalty: 0,
+      presence_penalty: 0,
     });
-    setResponse(completion.choices[0]?.message?.content);
-
+    const responseData = completion.choices[0]?.message?.content;
+    setResponse(responseData);
+    // const hasCodeBlock = responseData.includes("```");
+    // if (hasCodeBlock) {
+    //   const codeContent = responseData.replace(
+    //     /```([\s\S]+?)```/g,
+    //     "<code>$1</code>"
+    //   );
+    //   console.log(codeContent);
+    //   setResponse(codeContent);
+    // }
     console.log(completion);
   };
 
@@ -61,7 +85,16 @@ function App() {
       <button onClick={chatCompletion}>
         Upload File to get Rest Assured code
       </button>
-      {response && <p>{response}</p>}
+      {!response && clicked && (
+        <h2 style={{ display: "flex", alignItems: "center", gap: "1rem" }}>
+          Generating Code <DotPlus />
+        </h2>
+      )}
+      {response && (
+        <div className="output-plate">
+          <Markup content={response} />
+        </div>
+      )}
     </div>
   );
 }
---
+++ src/DotPlus.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const DotPlus = () => {
+  return (
+    <div className="col-3">
+      <div className="snippet" data-title="dot-pulse">
+        <div className="stage">
+          <div className="dot-pulse"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DotPlus;
