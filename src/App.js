375d4ecdc15088c33ceecd072c510ab3dace476d
shiba_1234 <shibajisahu121@gmail.com>
2024-03-03 20:22:37 +0530

    response fetch from open Ai working

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
+import OpenAI from "openai";
 import React, { useState } from "react";
 
 function App() {
   const [file, setFile] = useState(null);
   const [content, setContent] = useState(null);
+  const [response, setResponse] = useState(null);
 
   const handleFileChange = async (e) => {
     setFile(e.target.files[0]);
     const file = e.target.files.item(0);
     const text = await file.text();
     setContent(text);
+  };
+
+  const chatCompletion = async () => {
+    console.log("chat completion");
+    const openai = new OpenAI({
+      apiKey: process.env.REACT_APP_API_KEY,
+      dangerouslyAllowBrowser: true,
+    });
+    const completion = await openai.chat.completions.create({
+      messages: [{ role: "system", content: "You are a helpful assistant." }],
+      model: "gpt-3.5-turbo",
+    });
+    setResponse(completion.choices[0]?.message?.content);
+
+    console.log(completion.choices[0]?.message?.content);
   };
 
   return (
@@ -26,6 +43,8 @@ function App() {
           {content && <p>{content}</p>}
         </div>
       )}
+      <button onClick={chatCompletion}>Chat Completion</button>
+      {response && <p>{response}</p>}
     </div>
   );
 }
