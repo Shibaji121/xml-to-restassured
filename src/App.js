a9ac812477021727a0ba00c4da860a554a79268a
shiba_1234 <shibajisahu121@gmail.com>
2024-03-03 20:46:21 +0530

    file validations are added

--- src/App.js
+++ src/App.js
@@ -14,18 +14,34 @@ function App() {
   };
 
   const chatCompletion = async () => {
+    if (file === null) {
+      alert("Please select an XML file");
+      return;
+    }
+    if (file.type !== "text/xml") {
+      alert("Please select an XML file");
+      return;
+    }
+    if (!content) {
+      alert("Please select an XML file which contains the valid xml content");
+      return;
+    }
+    let additionalContent = content.concat(
+      "\n Convert XML content to Rest Assured JAVA Code"
+    );
+    setContent(additionalContent);
     console.log("chat completion");
     const openai = new OpenAI({
       apiKey: process.env.REACT_APP_API_KEY,
       dangerouslyAllowBrowser: true,
     });
     const completion = await openai.chat.completions.create({
-      messages: [{ role: "system", content: "You are a helpful assistant." }],
+      messages: [{ role: "system", content: content }],
       model: "gpt-3.5-turbo",
     });
     setResponse(completion.choices[0]?.message?.content);
 
-    console.log(completion.choices[0]?.message?.content);
+    console.log(completion);
   };
 
   return (
@@ -40,10 +56,11 @@ function App() {
           <p>File Type: {file.type}</p>
           <p>File Size: {file.size}</p>
           <p>Last Modified: {file.lastModifiedDate.toDateString()}</p>
-          {content && <p>{content}</p>}
         </div>
       )}
-      <button onClick={chatCompletion}>Chat Completion</button>
+      <button onClick={chatCompletion}>
+        Upload File to get Rest Assured code
+      </button>
       {response && <p>{response}</p>}
     </div>
   );
